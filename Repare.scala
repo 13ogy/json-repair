1900849a7b78fe9fa15fd0c8a967a8b1d2ce7794
mSETBEL <melsetbel@gmail.com>
2026-03-12 14:29:20 +0100

    feat: début de l'algorithme de réparation de type

--- Repare.scala
+++ Repare.scala
@@ -19,8 +19,41 @@ import io.circe.syntax._
 import cats.implicits._
 import scala.io.Source
 
+//validate renvoie un objet ValidationResult qui contient un booleen 'valid' et une liste d'erreurs de validation.
+//une erreur de validation est des informations sur l'emplacement de l'instance, l'emplacement du mot-clé et le message d'erreur.
+//repair utilise ces informations pour corriger les données en remplaçant les valeurs invalides par des valeurs par défaut basées sur le schéma.
 
-def repair(data: Json, schema: Json): Json =
+case class ValidationResult(
+  valid: Boolean,
+  errors: List[ValidationError]
+)
+
+case class ValidationError(
+  instanceLocation: String,
+  keywordLocation: String,
+  error: String
+)
+
+private def defaultValue(jsonType: String): Json = jsonType match {
+    case "string"  => "".asJson
+    case "integer" => 0.asJson
+    case "number"  => 0.0.asJson
+    case "boolean" => false.asJson
+    case "array"   => Json.arr()
+    case "object"  => Json.obj()
+    case _         => Json.Null
+  }
+
+
+def repairType(data: Json, schema: Json, error: ValidationError): Json =
   // definir Repair(J, S) by structral inductions. Start with string, number, object, arrays assertions. Assumption for objects: not editing the labels.
   
+  //pour chaque ValidationError, on la repare en utilisant les informations de l'erreur et le schéma
+  val path = error.instanceLocation.split("/").toList.tail
+  val keyword = error.keywordLocation.split("/").toList.tail.last
+  val jsonType = schema.hcursor.downField("properties").downField(path.head).downField("type").as[String].getOrElse("")
+  val defaultVal = defaultValue(jsonType)
+
+  // Repair the data by replacing the invalid value with the default value based on the schema
+  data.hcursor.downField(path.head).withFocus(_ => defaultVal).top.get
 
