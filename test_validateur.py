8e10b511575e7157f94ac6d26d008db6b185e53e
mSETBEL <melsetbel@gmail.com>
2026-03-12 14:29:20 +0100

    debut algo reparation de type

--- test_validateur.py
+++ test_validateur.py
@@ -3,24 +3,38 @@ from jschon import create_catalog, JSON, JSONSchema
 create_catalog('2020-12')
 
 demo_schema = JSONSchema({
-    "$id": "https://example.com/demo",
-    "$schema": "https://json-schema.org/draft/2020-12/schema",
-    "type": "array",
-    "items": {
-        "anyOf": [
-            {
-                "type": "string",
-                "description": "Cool! We got a string here!"
-            },
-            {
-                "type": "integer",
-                "description": "Hey! We got an integer here!"
-            }
-        ]
+  "$id": "https://example.com/person.schema.json",
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "title": "Person",
+  "type": "object",
+  "required": ["firstName", "lastName"],
+  "properties": {
+    "firstName": {
+      "type": "string",
+      "description": "The person's first name."
+    },
+    "lastName": {
+      "type": "string",
+      "description": "The person's last name."
+    },
+    "age": {
+      "description": "Age in years which must be equal to or greater than zero.",
+      "type": "integer",
+      "minimum": 0
     }
-})
+  }
+}
+
+)
 
 result = demo_schema.evaluate(
-    JSON(["True"])
+    JSON({
+        "firstName": "1",
+        "lastName": "True",
+        "age": 21
+        }
+
+    )
 )
+
 print(result.output('basic'))
